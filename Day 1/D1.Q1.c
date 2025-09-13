c66b90c2861b526699e48e5986c6a0b4f0aa8dfc
Himansh <himanshjeengar36@gmail.com>
2025-09-13 19:56:05 +0530

    Add files via upload

---
+++ Day 1/D1.Q1.c
@@ -0,0 +1,11 @@
+/* 100 days code; Day 1: code1*/
+#include<stdio.h>
+int main()
+{
+    printf("Himansh Jeengar\nSAP ID: 590025460\nCourse- BSc CS\nBatch-1");
+	printf("\n-------------------------------------------------------\n");
+    int a, b;
+    printf("Enter first and second number:"); scanf("%d%d", &a, &b);
+    printf("\nSum of the given numbers is: %d", a+b);
+    return 0;
+}
