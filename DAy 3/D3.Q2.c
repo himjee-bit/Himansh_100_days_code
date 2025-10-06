9632f6e77fde9e69f82709ee1e4678e725038140
Himansh <himanshjeengar36@gmail.com>
2025-10-06 22:23:54 +0530

    Delete DAy 3 directory

--- DAy 3/D3.Q2.c
+++
@@ -1,9 +0,0 @@
-/* 100 days code; Day 3: code2*/
-#include<stdio.h>
-int main()
-{
-	double r;
-	printf("Enter the radius of the circle:"); scanf("%lf", &r);
-	printf("The perimeter of the circle is: %lf", 2*3.141*r);
-	return 0;
-}
