a9ee9ea33732899750372ab2eba11550daaaa38e
Himansh <himanshjeengar36@gmail.com>
2025-10-06 22:28:53 +0530

    Add files via upload

---
+++ Day 6/D6.Q2.c
@@ -0,0 +1,14 @@
+/* 100 days code; Day 6: code2*/
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	double p, ci, l, r, n, k;
+	printf("Enter principal amount, rate, number of coumpounding periods" );
+	scanf("%lf %lf %lf",&p,&r,&n);
+	k=(1+(r/100));
+	l=pow(k,n);
+	ci=(p*l)-p;
+	printf("The compound interest interest is %lf", ci);
+	return 0;
+}
