3ba39128bac708da54ee078a2d6e4523cfa9616a
Himansh <himanshjeengar36@gmail.com>
2025-10-06 22:21:00 +0530

    Add files via upload

---
+++ DAy 3/D3.Q2.c
@@ -0,0 +1,9 @@
+/* 100 days code; Day 3: code2*/
+#include<stdio.h>
+int main()
+{
+	double r;
+	printf("Enter the radius of the circle:"); scanf("%lf", &r);
+	printf("The perimeter of the circle is: %lf", 2*3.141*r);
+	return 0;
+}
