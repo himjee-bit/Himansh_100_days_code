0ddddbab8a5f6710b4852902eea51eac88fb38b5
Himansh <himanshjeengar36@gmail.com>
2025-10-15 21:28:21 +0530

    Update D3.Q2.c

--- Day 3/D3.Q2.c
+++ Day 3/D3.Q2.c
@@ -2,8 +2,11 @@
 #include<stdio.h>
 int main()
 {
+	printf("Himansh Jeengar\nSAP ID: 590025460\nCourse: BSc. CS. \nBatch: 1");
+	printf("\n-----------------\n");
 	double r;
 	printf("Enter the radius of the circle:"); scanf("%lf", &r);
 	printf("The perimeter of the circle is: %lf", 2*3.141*r);
 	return 0;
 }
+
