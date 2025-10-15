2d6a648f5a621b8271d9babe95b1de7954bccbbf
Himansh <himanshjeengar36@gmail.com>
2025-10-15 21:28:46 +0530

    Update D4.Q1.c

--- Day 4/D4.Q1.c
+++ Day 4/D4.Q1.c
@@ -2,9 +2,12 @@
 #include<stdio.h>
 int main()
 {
+	printf("Himansh Jeengar\nSAP ID: 590025460\nCourse: BSc. CS. \nBatch: 1");
+	printf("\n-----------------\n");
 	float si, p, r, t, amt;
 	printf("Enter principal amount, rate and Time:"); scanf("%f %f %f", &p, &r, &t);
 	si=(p*r*t)/100; amt=p+si;
 	printf("Simple interest is: %f\nTotal amount is: %f", si, amt);
 	return 0;
 }
+
