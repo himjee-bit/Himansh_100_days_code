bab09fcb15757fefb740aed469da87134ae793d4
Himansh <himanshjeengar36@gmail.com>
2025-10-24 17:34:24 +0530

    Add files via upload

---
+++ Day 16/D16.Q1.c
@@ -0,0 +1,18 @@
+/* 100 days code; Day 16: code1*/
+#include<stdio.h>
+int main()
+{
+	printf("Himansh Jeengar\nSAP ID: 590025460\nCourse- BSc CS\nBatch-1"); 
+	printf("\n-----------------\n");
+	int i, j, n;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= i; j++)
+            printf("* ");
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ Day 16/D16.Q2.c
@@ -0,0 +1,20 @@
+/* 100 days code; Day 16: code2*/
+#include<stdio.h>
+int main()
+{
+	printf("Himansh Jeengar\nSAP ID: 590025460\nCourse- BSc CS\nBatch-1"); 
+	printf("\n-----------------\n");
+	int i, j, space, n;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        for (space = i; space < n; space++)
+            printf(" ");
+        for (j = 1; j <= i; j++)
+            printf("* ");
+        printf("\n");
+    }
+
+    return 0;
+}
